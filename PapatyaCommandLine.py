5382c2e532ef4561d4a6b145212b4e58777bec81
bbaransahin <barana340@gmail.com>
2022-01-25 22:16:58 +0300

    PapatyaModel and PapatyaLayer classes added.

--- PapatyaCommandLine.py
+++ PapatyaCommandLine.py
@@ -1,22 +1,50 @@
 import tensorflow as tf
 
+class PapatyaLayer:
+    def __init__(self, name, kind):
+        self.name = name
+        self.kind = kind
+    def sendOutputTo(self, nextLayer):
+        self.nextLayer = nextLayer
+    def getInputFrom(self, previousLayer):
+        self.previousLayer = previousLayer
+
+class PapatyaModel:
+    def __init__(self, name):
+        print("A PapatyaModel named "+name+" is initialized.")
+        self.name = name
+        self.layers = []
+    def addLayer(self, pLayer):
+        self.layers.append(pLayer)
+    def build(self):
+        pass
+
 class PapatyaCLI:
     def __init__(self):
         print("PapatyaCLI v1")
         self.layers = []
-    def executeCommand(command):
+    def executeCommand(self, command):
         command = command.split(' ')
         if(command[0] == "help"):
             pass
         elif(command[0] == "add"):
             if(command[1] == "layer"):
                 if(command[2] == "input"):
-                    pass
+                    inputs = tf.keras.Input(shape=self.getShape(command[3]))
         elif(command[0] == "init"):
             pass
-    def getShape(shape):
+        elif(command[0] == "exit"):
+            isRun = False
+    def getShape(self, shape):
         shape = shape.split('x')
         returnShape = ()
         for i in shape:
             returnShape = shape + (i,)
-        returnShape
+        return shape
+
+isRun = True
+cli = PapatyaCLI()
+
+while(isRun):
+    userInput = input(">>")
+    cli.executeCommand(userInput)
