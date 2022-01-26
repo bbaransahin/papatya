2269918e023a420b1fdec150bb76a621983712ce
bbaransahin <barana340@gmail.com>
2022-01-26 19:39:53 +0300

    implemented some search functions and todo item

--- PapatyaCommandLine.py
+++ PapatyaCommandLine.py
@@ -1,9 +1,11 @@
+#TODO: implement build function of papatya model
 import tensorflow as tf
 
 class PapatyaLayer:
-    def __init__(self, name, kind):
+    def __init__(self, name, kind, kerasLayer):
         self.name = name
         self.kind = kind
+        self.kerasLayer = kerasLayer
     def sendOutputTo(self, nextLayer):
         self.nextLayer = nextLayer
     def getInputFrom(self, previousLayer):
@@ -16,6 +18,16 @@ class PapatyaModel:
         self.layers = []
     def addLayer(self, pLayer):
         self.layers.append(pLayer)
+    def connectTwoLayer(self, pLayer0, pLayer1):
+        player1 = player1(pLayer1)
+    def findLayerByName(self, name):
+        for l in self.layers:
+            if(l.name == name):
+                return l
+    def findLayerByKind(kind):
+        for l in self.layers:
+            if(l.kind == kind):
+                return l
     def build(self):
         pass
 
