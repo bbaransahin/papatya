ed9b4c7c1e209fb6d1b32d7592064bc8878c3af7
bbaransahin <barana340@gmail.com>
2022-02-01 16:03:48 +0300

    build function implemented and exit command bug is noted

--- PapatyaCommandLine.py
+++ PapatyaCommandLine.py
@@ -1,4 +1,4 @@
-#TODO: implement build function of papatya model
+#BUG: There is a bug in exit command
 import tensorflow as tf
 
 class PapatyaLayer:
@@ -8,6 +8,8 @@ class PapatyaLayer:
         self.name = name
         self.kind = kind
         self.kerasLayer = kerasLayer
+        self.nextLayer = None
+        self.previousLayer = None
     def sendOutputTo(self, nextLayer):
         self.nextLayer = nextLayer
     def getInputFrom(self, previousLayer):
@@ -21,22 +23,28 @@ class PapatyaModel:
     def addLayer(self, pLayer):
         self.layers.append(pLayer)
     def connectTwoLayer(self, pLayer0, pLayer1):
-        player1 = player1(pLayer1)
+        pLayer1.kerasLayer = pLayer1.kerasLayer(pLayer0.kerasLayer)
     def findLayerByName(self, name):
         for l in self.layers:
             if(l.name == name):
                 return l
-    def findLayerByKind(kind):
+    def findLayerByKind(self, kind):
         for l in self.layers:
             if(l.kind == kind):
                 return l
     def build(self):
-        pass
+        self.inputs = self.findLayerByKind("input").kerasLayer
+        for l in self.layers:
+            if(not l.nextLayer == None):
+                self.connectTwoLayer(l,self.findLayerByName(l.nextLayer))
+        self.outputs = self.findLayerByKind("output").kerasLayer
+        self.kerasModel = tf.keras.Model(inputs=self.inputs, outputs=self.outputs)
+        print(self.kerasModel.summary())
 
 class PapatyaCLI:
     def __init__(self):
         print("PapatyaCLI v1")
-        pModel = PapatyaModel(input("Papatya Model Name: "))
+        self.pModel = PapatyaModel(input("Papatya Model Name: "))
     def executeCommand(self, command):
         command = command.split(' ')
         if(command[0] == "help"):
@@ -46,21 +54,30 @@ class PapatyaCLI:
                 if(command[2] == "input"):
                     inputs = tf.keras.Input(shape=self.getShape(command[3]))
                     pLayer = PapatyaLayer(input("layer name: "), "input", inputs)
-                    pModel.addLayer(pLayer)
-                elif(comman[2] == "dense"):
+                    self.pModel.addLayer(pLayer)
+                elif(command[2] == "dense"):
                     dense = tf.keras.layers.Dense(int(command[3]), activation="relu")
                     pLayer = PapatyaLayer(input("layer name: "), "hidden", dense)
-                    pModel.addLayer(pLayer)
+                    self.pModel.addLayer(pLayer)
+                elif(command[2] == "output"):
+                    output = tf.keras.layers.Dense(int(command[3]), activation="softmax")
+                    pLayer = PapatyaLayer(input("layer name: "), "output", output)
+                    self.pModel.addLayer(pLayer)
+        elif(command[0] == "connect" and command[2] == "to"):
+            self.pModel.findLayerByName(command[1]).sendOutputTo(command[3])
+            self.pModel.findLayerByName(command[3]).getInputFrom(command[1])
+        elif(command[0] == "build"):
+            self.pModel.build()
         elif(command[0] == "init"):
             pass
         elif(command[0] == "exit"):
             isRun = False
     def getShape(self, shape):
         shape = shape.split('x')
-        returnShape = ()
+        returnShape = []
         for i in shape:
-            returnShape = shape + (i,)
-        return shape
+            returnShape.append(int(i))
+        return returnShape
 
 isRun = True
 cli = PapatyaCLI()
