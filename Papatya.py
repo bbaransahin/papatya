ca74a01867643e335f98f0a98a6b0096c2a68459
bbaransahin <barana340@gmail.com>
2022-02-12 02:19:15 +0300

    GUI base created and some classes are initialized (button, entrybox, frame..)

--- Papatya.py
+++ Papatya.py
@@ -1,3 +1,4 @@
+from enum import Flag
 import pygame
 
 import tensorflow as tf
@@ -7,3 +8,58 @@ sys.path.append(".")
 from PapatyaClasses import PapatyaModel
 from PapatyaClasses import PapatyaLayer
 from PapatyaClasses import PapatyaKernel
+
+import pygame
+
+class ScreenObject:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+    def blit(self):
+        pass
+
+class Button(ScreenObject):
+    def __init__(self, name, x, y, width, height, text):
+        super().__init__(name, x, y)
+        self.width = width
+        self.height = height
+        self.text = text
+        self.fontSize = 30
+    def blit(self, screen):
+        font = pygame.font.Font("freesansbold.ttf",self.fontSize)
+        pygame.draw.rect(screen, (150,150,230), self.x, self.y, self.width, self.height)
+        screen.blit(font.render(self.text, True, (50,50,50)), (self.x+5, self.y+5))
+
+class EntryBox(ScreenObject):
+    def __init__(self, name, x, y, width, height):
+        super().__init__(name, x, y)
+        self.width = width
+        self.height = height
+        self.text = ""
+    def blit(self, screen):
+        pygame.draw.rect(screen, (220,220,220), self.x, self.y, self.width, self.height)
+    def appendToText(self, ch):
+        self.text+=ch
+
+class Frame:
+    def __init__(self,title):
+        self.title = title
+        self.objects = []
+    def blit(self, screen):
+        for obj in self.objects:
+            obj.blit(screen)
+    def addObj(self, obj):
+        self.objects.append(obj)
+
+pygame.init()
+screen = pygame.display.set_mode((1280,720))
+isRun = True
+FPS = 60
+fpsClock = pygame.time.Clock()
+currentFrame = None
+
+while(isRun):
+    for event in pygame.event.get():
+        if(event.type == pygame.QUIT):
+            isRun = False
